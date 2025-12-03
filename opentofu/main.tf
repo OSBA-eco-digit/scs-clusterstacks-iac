14ace3a002756e94fc5bcb7eda335f6656ad2d41
Vinícius Zavam <zavam@osb-alliance.com>
2025-12-03 08:25:31 -0300

    bump the sleep timer

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -187,7 +187,7 @@ resource "local_file" "my_ansible_inventory" {
 resource "null_resource" "my_provisioner" {
   triggers = { my_instance_public_addr4 = openstack_networking_floatingip_v2.my_floatingip4.address }
   provisioner "local-exec" {
-    command = "sleep 13; ANSIBLE_CONFIG=${var.AC} ANSIBLE_ROLES_PATH=${var.ARP} ansible-playbook -i ../ansible/inventory_${var.OS_CLOUD}.ini ../ansible/playbooks/scs-cluster-stack.yml"
+    command = "sleep 30; ANSIBLE_CONFIG=${var.AC} ANSIBLE_ROLES_PATH=${var.ARP} ansible-playbook -i ../ansible/inventory_${var.OS_CLOUD}.ini ../ansible/playbooks/scs-cluster-stack.yml"
   }
   depends_on = [
     openstack_networking_floatingip_associate_v2.my_floatingip4_associate
